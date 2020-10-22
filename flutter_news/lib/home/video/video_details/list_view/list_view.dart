829c465ba73c088d8ae72c1da33fb1f8440bd770
zhugy781 <zhugy781@163.com>
2020-10-22 15:17:44 +0800

    构建列表页

---
+++ flutter_news/lib/home/video/video_details/list_view/list_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ListView extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ListView();
+  }
+}
+
+class _ListView extends State<ListView> {
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+
+}
